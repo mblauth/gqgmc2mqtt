415fc5f3f4de45d7c6dd96dd1777e2405ca4fc6b
Marvin Blauth <marvin@slatanic.org>
2022-10-11 16:16:57 +0200

    Parameterize options

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import signal
 import time
 import paho.mqtt.client
+import argparse
 
 import serial
 
@@ -16,21 +17,33 @@ def interrupt(sig, frame):
 
 
 def main():
+    parser = argparse.ArgumentParser(description="send GQ GMC events to MQTT")
+    parser.add_argument("-broker", type=str, nargs=1, required=True,
+                        help="the hostname or IP address of the MQTT broker")
+    parser.add_argument("-port", type=int, nargs=1, default=1883,
+                        help="the port of the MQTT broker (default: 1883)")
+    parser.add_argument("-topic", type=str, nargs=1, required=True,
+                        help="the MQTT topic on which to publish")
+    parser.add_argument("-baudrate", type=int, nargs=1, default=57600,
+                        help="the baudrate of the serial interface (default: 57600)")
+    parser.add_argument("-serial", type=str, nargs=1, default="/dev/ttyUSB0",
+                        help="the serial interface to the GQ GMC Geiger Counter (default: /dev/ttyUSB0)")
+    args = parser.parse_args()
     global con
-    con = serial.Serial("/dev/ttyUSB0", 57600)
+    con = serial.Serial(args.serial, args.baudrate)
     con.write("<GETVER>>".encode())
     version = con.read(14)
     print(version.decode())
-    signal.signal(getattr(signal, 'SIGINT'), interrupt)
+    signal.signal(getattr(signal, "SIGINT"), interrupt)
 
     mqtt = paho.mqtt.client.Client("mqtt")
-    mqtt.connect("10.0.138.117", 1883)
+    mqtt.connect(args.broker, args.port)
 
     while True:
         con.write("<GETCPM>>".encode())
         value = ord(con.read(1))+ord(con.read(1))
         print(value)
-        mqtt.publish("radiation/arbeitszimmer", value)
+        mqtt.publish(parser.topic, value)
         time.sleep(60)
 
 
